af075430b74a0cba2ef3977d2763a4f1c673d510
Kusum Negi <kusumnegi2006@gmail.com>
2020-08-15 21:08:19 +0200

    Database accessible from remote

---
+++ README.md
@@ -0,0 +1,12 @@
+# H2-connect 
+
+### H2 Database
+All DDL and DML drafted within `data.sql` and `schema.sql` will be executed when application will start:
+
+### H2 Database Access
+Access database on `http://localhost:8080/h2` with data drafted within `application.yml` file.
+
+### H2 Remote access
+Access database using below JDBC URL:
+ 1. Windows: `jdbc:h2:tcp://localhost:8080//C://Users//<USER>//database//mydb`
+ 2. Linux: `jdbc:h2:tcp://188.76.64.224:1512//home//<USER>//database//mydb`
--- src/main/java/com/h2/database/DatabaseApplication.java
+++ src/main/java/com/h2/database/DatabaseApplication.java
@@ -1,13 +1,25 @@
 package com.h2.database;
 
+import org.h2.tools.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class DatabaseApplication {
+
+	@Value("${spring.h2.port}")
+	private String h2DatabasePort;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatabaseApplication.class, args);
 	}
 
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public Server h2Server() throws SQLException {
+		return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", h2DatabasePort);
+	}
 }
